efd527d4a96ff196e3a1981b8e1ade11383d0b66
Mikey Lau <mikey@3sidedcube.com>
2024-05-30 21:05:54 +0100

    refactor(plugin): remove snippets

--- lua/mikeylau/plugins/cmp.lua
+++ lua/mikeylau/plugins/cmp.lua
@@ -4,34 +4,16 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
-		{
-			"L3MON4D3/LuaSnip",
-
-			version = "v2.*",
-
-			build = "make install_jsregexp",
-		},
-		"saadparwaiz1/cmp_luasnip",
-		"rafamadriz/friendly-snippets",
 		"onsails/lspkind.nvim",
 	},
 	config = function()
 		local cmp = require("cmp")
 
-		local luasnip = require("luasnip")
-
 		local lspkind = require("lspkind")
-
-		require("luasnip.loaders.from_vscode").lazy_load()
 
 		cmp.setup({
 			completion = {
 				completeopt = "menu,menuone,preview,noselect",
-			},
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<s-tab>"] = cmp.mapping.select_prev_item(),
@@ -44,7 +26,6 @@ return {
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
 				{ name = "buffer" },
 				{ name = "path" },
 			}),

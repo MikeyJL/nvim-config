58add55beded756ca694862ba37f337f09e1574b
Mikey Lau <mikey@3sidedcube.com>
2024-05-31 19:26:42 +0100

    refactor(plugin): remove barbar
    Use telescope buffers instead

--- lua/mikeylau/plugins/telescope.lua
+++ lua/mikeylau/plugins/telescope.lua
@@ -42,7 +42,7 @@ return {
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files in cwd" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 	end,
 }

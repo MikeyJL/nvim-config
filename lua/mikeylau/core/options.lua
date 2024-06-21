f94ba7716e08c7b524a3bd2d4f59e4163c7e4a05
Mikey Lau <mikey@3sidedcube.com>
2024-06-21 20:53:36 +0100

    refactor(plugin): remove nvim-tree

--- lua/mikeylau/core/options.lua
+++ lua/mikeylau/core/options.lua
@@ -1,27 +1,30 @@
-vim.cmd("let g:netrw_liststyle = 3")
+local g = vim.g
 
-local opt          = vim.opt
+g.netrw_banner = 0
+g.netrw_liststyle = 3
+
+local opt = vim.opt
 
 opt.relativenumber = true
-opt.number         = true
+opt.number = true
 
-opt.tabstop        = 2
-opt.shiftwidth     = 2
-opt.expandtab      = true
-opt.autoindent     = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
 
-opt.wrap           = false
+opt.wrap = false
 
-opt.ignorecase     = true
-opt.smartcase      = true
+opt.ignorecase = true
+opt.smartcase = true
 
-opt.cursorline     = true
+opt.cursorline = true
 
-opt.termguicolors  = true
-opt.background     = "dark"
-opt.signcolumn     = "yes"
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
 
-opt.backspace      = "indent,eol,start"
+opt.backspace = "indent,eol,start"
 
 opt.clipboard:append("unnamedplus")
 
